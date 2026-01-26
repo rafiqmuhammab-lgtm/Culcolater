f56fbebb09d6b972cf3ecfc36bfafabec95e462c
rafiqmuhammab-lgtm <rafiqmuhammab@gmail.com>
2026-01-26 15:02:28 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const cacheName = 'const-v4';
+const assets = ['./', './index.html', './manifest.json', './icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
